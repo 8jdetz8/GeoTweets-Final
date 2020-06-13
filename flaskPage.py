4890054c03db1fc0b30039cc15cb1e6a97d5b069
8jdetz8 <8jdetz8@gmail.com>
2020-06-13 08:33:02 -0400

    flask change

--- flaskPage.py
+++ flaskPage.py
@@ -14,20 +14,19 @@ def second():
     return render_template('geoTweetsPage.html')
 
 
-@app.route('/data2')
+@app.route('/data2', methods = ["POST"])
 def third():
     import subprocess
     arg3 = int(request.data["third"])
     if arg3 == 1:
-        // get
-        output
-        with arg one
+        arg3
     else:
         arg4 = request.data["fourth"]
-        // get
-        output
-        with arg one and four
+
 
     output = subprocess.check_output(
-    ['java', 'C:\\Users\\Student\\SDE\\4thTryGeoTweets\\src\\main\\java\\geotweets', 'all_tweets.txt', 'states.csv'])
+    ['java', 'C:\\Users\\Student\\SDE\\4thTryGeoTweets\\src\\main\\java\\geotweets', 'all_tweets.txt', 'states.csv', arg3, arg4])
     return output
+
+
+app.run(host='127.0.0.1', port=5000)
